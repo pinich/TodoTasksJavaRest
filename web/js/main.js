584c3eb36f1e9feb87a5340177675d8b2e7003ed
Pini Cheyni <pinich@gmail.com>
2017-03-21 18:29:30 +0200

    - ADD, done , delete operations working - Edit with blur needs to be finished

--- web/js/main.js
+++ web/js/main.js
@@ -4,107 +4,154 @@ var user_post = null;
 
 //this function is take care when the browser is load at the beginning
 $(document).ready(function () {
-    user_d = getLocalStorageVal("user"); //get user details from local storage
-    if (user_d !== null)
-    {
-        user_d = JSON.parse(user_d); //make user_d a json object
-        updateLocalStorage();
-    }else{
-        homePage(); //go to home page
-    }
-    
+    fillTodoList();
 });
 
-function updateLocalStorage() {
-    $.ajax({
-        url: "api/user/getUserByID/" + user_d.id,
-        type: "GET",
-        dataType: "json"
-    }).done(function (json) {
-        var data = json["data"];
-        console.log(data);
 
-        user_d["email"] = data["email"];
-        user_d["active"] = data["active"];
-        user_d["firstName"] = data["firstName"];
-        user_d["lastName"] = data["lastName"];
-        user_d["picture"] = data["picture"];
-        setLocalStorageVal("user", JSON.stringify(user_d));
-        
-        homePage(); //go to home page
-    })
-            .fail(function (xhr, status, errorThrown) {
-                console.log("Error: Unable to load user data");
-            });
-}
-
-
-//Checks whether the user is logged in
-function userIsLoggedIn() {
-    return (user_d !== null);
-}
-
-//Loads the home page: if user is logged in and activated show posts list, if not activated show message, if not logged in show registration.
-function homePage() {
-    fillNavBar();
-    if (userIsLoggedIn()) {
-        if (user_d["active"] === "1") { //if user logged in and ACTIVATED
-
-            fillHomePage();
-        } else { //if user is logged in but account is NOT ACTIVATED
-            $("#page").html("<div id='activate'>  <a href='javascript:checkDeactiveActivate(); ' >Activate Your Account</a></div> ");
-        }
-    } else { //if user is not registered
-        fillRegister();
-    }
-}
-
-//Loads everything that's relevant to profile page
-function profilePage() {
-
-    fillProfileDetails();
-
-}
-//Loads everything that's relevant to the change password page
-function changePasswordPage() {
-    fillChangePass();
-}
 
 // Gets the homePage template, puts it in document, then fills it with add post template and posts list
-function fillHomePage() {
+function fillTodoList() {
     $.ajax({
-        url: "templates/homePage.html",
+        url: "templates/listELM.html",
         type: "GET"
     })
             .done(function (html) {
-                $("#page").html(html); //put homePage template in div with id  "page" 
-                putAddPostTemplate(); //put add post template
-                fillPostList(); //fill post list with posts from all users
+                getTodosData(html);
             })
             .fail(function (xhr, status, errorThrown) {
                 console.log("Error: Unable to load postsList");
             });
 }
 
-//Gets key and retrieves the value of the key in local storage
-function getLocalStorageVal(key) {
-    if (typeof (Storage) !== "undefined") {
-        // Code for localStorage/sessionStorage.
-        return localStorage.getItem(key);
-    } else {
-        // Sorry! No Web Storage support..
-        return null;
-    }
+function getTodosData(template) {
+    $.ajax({
+        url: "api/todo",
+        type: "GET",
+        dataType: 'json',
+        contentType: 'application/json',
+    })
+            .done(function (data) {
+                data = data["data"];
+                var todoList = $(".list-group");
+                todoList.html('');
+                for (var i = 0; i < data.length; i++) {
+                    let todo = data[i];
+                    let tmp = $(template);
+                    tmp.find(".todoMessage").text(todo["message"]);
+                    tmp.find(".todoEditBtn").attr("onclick", "editTodoStart('" + todo.id + "');return false;");
+                    tmp.find(".todoDeleteBtn").attr("onclick", "deleteTodo('" + todo.id + "');return false;");
+                    tmp.attr("id", "todo_" + todo.id);
+                    tmp.attr("href", "javascript:todoDone('" + todo.id + "');")
+                    if (todo["status"] == 1) {
+                        tmp.addClass("list-group-item-success");
+                    }
+                    todoList.append(tmp); //append filled post to the postslist element
+                }
+            })
+            .fail(function (xhr, status, errorThrown) {
+                console.log("Error: Unable to load postsList");
+            });
 }
 
-//Gets key and value and stores it in local storage
-function setLocalStorageVal(key, value) {
-    if (typeof (Storage) !== "undefined") {
-        // Code for localStorage/sessionStorage.
-        localStorage.setItem(key, value);
-        return true;
-    } else {
-        // Sorry! No Web Storage support..
-        return null;
+function editTodoStart(id) {
+    var todoELM = $("#todo_" + id);
+    console.log("Editing:" + id);
+    //EDIT
+    var switchToInput = function () {
+        var $input = $("<input>", {
+            val: $(this).text(),
+            type: "text",
+            rel: jQuery(this).text(),
+        });
+        $input.addClass("loadNum");
+        $(this).replaceWith($input);
+        $input.on("blur", switchToSpan);
+        $input.select();
+    };
+    var switchToSpan = function () {
+        if (jQuery(this).val()) {
+            var $text = jQuery(this).val();
+        } else {
+            var $text = jQuery(this).attr('rel');
+        }
+        var $span = $("<span>", {
+            text: $text,
+        });
+        $span.addClass("loadNum");
+        $(this).replaceWith($span);
+        $span.on("click", switchToInput);
     }
+
+
+}
+
+function deleteTodo(id) {
+    console.log("Delete:" + id);
+    var todoELM = $("#todo_" + id);
+    $.ajax({
+        url: "api/todo",
+        type: "DELETE",
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify({"id": id})
+    })
+            .done(function (html) {
+                todoELM.remove();
+            })
+            .fail(function (xhr, status, errorThrown) {
+                console.log("Error: Unable to load postsList");
+            });
+}
+
+function addTodo() {
+    let dataToSend = {
+        "message": $("#txtInput").val(),
+        "status": 0,
+        "other": ""
+    }
+    $.ajax({
+        url: "api/todo",
+        type: "POST",
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(dataToSend)
+    })
+            .done(function (html) {
+                fillTodoList(html);
+            })
+            .fail(function (xhr, status, errorThrown) {
+                console.log("Error: Unable to load postsList");
+            });
+}
+
+function todoDone(id) {
+    console.log("Todo Done" + id);
+    var todoELM = $("#todo_" + id);
+
+    var taskStatus = -1;
+    if (todoELM.hasClass("list-group-item-success")) {
+        taskStatus = 0;
+    } else {
+        taskStatus = 1;
+    }
+
+    var dataToSend = {
+        "id": id,
+        "message": todoELM.find(".todoMessage").text(),
+        "status": taskStatus,
+        "other": ""
+    }
+    $.ajax({
+        url: "api/todo",
+        type: "PUT",
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(dataToSend)
+    })
+            .done(function (html) {
+                todoELM.toggleClass("list-group-item-success");
+            })
+            .fail(function (xhr, status, errorThrown) {
+                console.log("Error: Unable to load postsList");
+            });
 }
