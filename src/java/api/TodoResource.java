4f48dc300e20c5a4d54dc3846f28f611eebd8833
Pini Cheyni <pinich@gmail.com>
2017-03-21 16:21:01 +0200

    - Back End Finished

--- src/java/api/TodoResource.java
+++ src/java/api/TodoResource.java
@@ -7,14 +7,21 @@ package api;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import db.DAL;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import model.Todo;
 
 /**
  * REST Web Service
@@ -42,7 +49,7 @@ public class TodoResource {
     public Response getAllPosts() {
         JsonObject result = new JsonObject();
         //JsonArray posts = DAL.getAllPosts();
-        JsonArray posts = new JsonArray();
+        JsonArray posts = DAL.getAllTodos();
         result.add("data", posts);
         result.addProperty("status", 0);
         result.addProperty("msg", "Success");
@@ -51,16 +58,85 @@ public class TodoResource {
     
     
     @GET
-    @Path("ent/user_id/{idValue}")
+    @Path("/{idValue}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getPostsByUserId(@PathParam("idValue") String idValue) {
+    public Response getTodoById(@PathParam("idValue") String idValue) {
         JsonObject result = new JsonObject();
-        //JsonArray posts = DAL.getPostsByUserId(idValue);
-        JsonArray posts = new JsonArray();
-        result.add("data", posts);
+        JsonObject todos = DAL.getTodoById(idValue);
+        result.add("data", todos);
         result.addProperty("status", 0);
         result.addProperty("msg", "");
         return Response.status(200).entity(result.toString()).build();
     }
     
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(String content) {
+        JsonObject o = (JsonObject) new JsonParser().parse(content);
+        JsonObject result = new JsonObject();
+        
+        JsonObject tmp = DAL.getTodoById(o.get("id").getAsString());
+        
+        Todo u = new Todo(tmp.get("id").getAsString(),o.get("message").getAsString(),o.get("status").getAsInt(),tmp.get("date").getAsString(),o.get("other").getAsString());
+        
+        String res = DAL.updateTodo(u);
+        if (!res.equals("-1")) {
+            result.addProperty("data", res);
+            result.addProperty("status", 0);
+            result.addProperty("msg", "");
+        } else {
+            result.addProperty("msg", "Unable to update Todo");
+            result.addProperty("status", -1);
+        }
+        return Response.status(200).entity(result.toString()).build();
+    }
+    
+    
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response insert(String content) {
+        JsonObject o = (JsonObject) new JsonParser().parse(content);
+        JsonObject result = new JsonObject();
+        
+        Todo u = new Todo("-1",o.get("message").getAsString(),o.get("status").getAsInt(),"",o.get("other").getAsString());
+        
+        String res = DAL.insertTodo(u);
+        if (!res.equals("-1")) {
+            result.addProperty("data", res);
+            result.addProperty("status", 0);
+            result.addProperty("msg", "");
+        } else {
+            result.addProperty("msg", "Unable to Insert Todo");
+            result.addProperty("status", -1);
+        }
+        return Response.status(200).entity(result.toString()).build();
+    }
+    
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(String content) {
+        JsonObject o = (JsonObject) new JsonParser().parse(content);
+        JsonObject result = new JsonObject();
+        
+        JsonObject tmp = DAL.getTodoById(o.get("id").getAsString());
+        String res;
+        if(tmp.has("id")){          //check if found
+            res = DAL.deleteTodoById(tmp.get("id").getAsString());
+        }else{
+            res="-1";
+        }
+        if (!res.equals("-1")) {
+            result.addProperty("data", res);
+            result.addProperty("status", 0);
+            result.addProperty("msg", "");
+        } else {
+            result.addProperty("msg", "Unable to Delete Todo");
+            result.addProperty("status", -1);
+        }
+        return Response.status(200).entity(result.toString()).build();
+    }
 }
--- src/java/db/DAL.java
+++ src/java/db/DAL.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package db;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import java.util.HashMap;
-import java.util.Map;
 import model.*;
 
 
@@ -19,148 +11,57 @@ import model.*;
  */
 public class DAL {
 
-    public static String mySqlConnectionString = "jdbc:mysql://pinich.ddns.net/dbPhoto?user=admin&password=admin1234";
-//    public static String mySqlConnectionString = "jdbc:mysql://localhost/dbPhoto?user=root&password=1";
+    public static String mySqlConnectionString = "jdbc:mysql://localhost/test?user=root&password=";
 
-    public static String test() {
-        String query = "SELECT * FROM `test`";
+    public static JsonArray getAllTodos() {
+        String query = "Select * FROM `todo`";
         DataBase db = DataBase.getInstance();
-        JsonArray result = db.getRSasJsonArray(query);
-        return "Output from Test DAL: " + result.size();
+        JsonArray rslt = db.getRSasJsonArray(query);
+        return rslt;
     }
 
-    
-    /**
-     * Insert new comment into the DataBase, and change the ID of the object
-     *
-     * @param c - Comment
-     * @return New ID
-     */
-    public static String insertComment(Comment c) {
+    public static String insertTodo(Todo c) {
         String output;
-        String query = "INSERT INTO `comment` ( `body`, `date`, `user_id`, `post_id`) "
-                + "VALUES ( '" + c.getBody() + "', CURRENT_TIMESTAMP, '" + c.getUser_id() + "', '" + c.getPost_id() + "')";
+        String query = "INSERT INTO `todo` ( `message`, `date`, `status`) "
+                + "VALUES ( '" + c.getMessage()+ "', CURRENT_TIMESTAMP, '" + c.getStatus() + "')";
         DataBase db = DataBase.getInstance();
         output = String.valueOf(db.Insert(query));
         c.setId(output);
         return output;
     }
-
-    /**
-     * Gets Post ID and returns Hashmap of all the comments by this post ID, The
-     * key is the Comment ID
-     *
-     * @param id The Post ID
-     * @return HashMap of all the comments by PostID
-     */
-    public static JsonArray getCommentsByPostId(String id) {
-        String query = "Select comm.* , `user`.`username` "
-                + "FROM (SELECT * FROM `comment` WHERE `post_id` = '" + id + "') comm "
-                + "LEFT JOIN `user` "
-                + "ON comm.`user_id` = `user`.`id`";
+    
+    public static String deleteTodoById(String id) {
+        String output;
+        String query = "DELETE FROM `todo` WHERE `id` = '" + id + "'";
         DataBase db = DataBase.getInstance();
-        JsonArray rslt = db.getRSasJsonArray(query);
-        return rslt;
-    }
-
-
-
-    //<editor-fold defaultstate="collapsed" desc="User DAL">
-
-    /**
-     * Get user by a given user id
-     *
-     * @param id
-     * @return User object (with a given id), of null if user with given id was
-     * not found
-     */
-    public static User getUserById(String id) {
-        User output = null;
-        String query = "Select * FROM `user` WHERE `id` = '" + id + "'";
-        DataBase db = DataBase.getInstance();
-        JsonArray rslt = db.getRSasJsonArray(query);
-        if (rslt.size() > 0) {
-            JsonObject o = (JsonObject) rslt.get(0);
-            output = new User(o.get("id").getAsString(), o.get("username").getAsString(), o.get("pass").getAsString(), o.get("email").getAsString(), o.get("reg_date").getAsString(), o.get("active").getAsString(), o.get("firstName").getAsString(), o.get("lastName").getAsString(), o.get("birthDay").getAsString(), o.get("picture").getAsString());
-        }
+        output = String.valueOf(db.Delete(query));
         return output;
     }
-
-    /**
-     * Insert given user into database
-     *
-     * @param u of type User
-     * @return number of inserted rows, or -1 if there was a problem
-     */
-    public static String insertUser(User u) {
+    
+    public static String updateTodo(Todo c) {
         String output;
-        String query = "INSERT INTO `user` ( `username`, `pass`, `email`,`firstName`,`lastName`,`birthDay`,`picture`) "
-                + "VALUES ( '" + u.getUserName() + "', '" + u.getPass() + "', '" + u.getEmail() + "','" + u.getFirstName() + "','" + u.getLastName() + "','" + u.getBirthDay() + "','" + u.getPicture() + "')";
-        DataBase db = DataBase.getInstance();
-        output = String.valueOf(db.Insert(query));
-        u.setId(output);
-        return output;
-    }
-
-
-    /**
-     * Get all users from database as Json array
-     *
-     * @return Json array that contains all the users in database
-     */
-    public static JsonArray getAllUsers() {
-        String query = "Select * FROM `user`";
-        DataBase db = DataBase.getInstance();
-        JsonArray rslt = db.getRSasJsonArray(query);
-        return rslt;
-    }
-
-
-//</editor-fold>
-    /**
-     * Update user password in database from given User object (by id)
-     *
-     * @param u
-     * @return number of updated rows in database, or -1 if fails
-     */
-    public static String updatePassword(User u) {
-        String output;
-        String query = "UPDATE `user` SET "
-                + "`pass`= '" + u.getPass() + "' "
-                + "WHERE `id` = '" + u.getId() + "'";
+        String query = "UPDATE `todo` SET "
+                + "`message`= '" + c.getMessage()+ "' ,"
+                + "`status`= '"+ c.getStatus() +"' ,"
+                + "`other`= '"+ c.getOther()+"' "
+                + "WHERE `id` = '" + c.getId() + "'";
         DataBase db = DataBase.getInstance();
         output = String.valueOf(db.Update(query));
         return output;
     }
 
-
-
-    public static String updateProfilePic(User u) {
-        String output;
-        String query = "UPDATE `user` SET "
-                + "`picture`= '" + u.getPicture() + "' "
-                + "WHERE `id` = '" + u.getId() + "'";
+    public static JsonObject getTodoById(String id) {
+        String query = "SELECT * FROM `todo` WHERE `id` = '" + id + "'";
         DataBase db = DataBase.getInstance();
-        output = String.valueOf(db.Update(query));
-        return output;
-    }
-
-//</editor-fold>
-    /**
-     * User userPost by username
-     *
-     * @param username
-     * @return User object, or null if there was no match
-     */
-    public static User userPost(String username) {
-        User output = null;
-        String query = "Select * FROM `user` WHERE `username`='" + username + "'";
-        DataBase db = DataBase.getInstance();
-        JsonArray rslt = db.getRSasJsonArray(query);
-        if (rslt.size() > 0) {
-            JsonObject o = (JsonObject) rslt.get(0);
-            output = new User(o.get("id").getAsString(), o.get("username").getAsString(), o.get("pass").getAsString(), o.get("email").getAsString(), o.get("reg_date").getAsString(), o.get("active").getAsString(), o.get("firstName").getAsString(), o.get("lastName").getAsString(), o.get("birthDay").getAsString(), o.get("picture").getAsString());
+        JsonArray rsltArr = db.getRSasJsonArray(query);
+        JsonObject rslt;
+        if(rsltArr.size()>0){
+            rslt = rsltArr.get(0).getAsJsonObject();
+        }else{
+            rslt = new JsonObject();
         }
-        return output;
+        return rslt;
     }
+    
+    
 }
